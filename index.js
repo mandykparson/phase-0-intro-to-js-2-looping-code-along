dca7cb9ba6830ba4906da278e9f568ce5b9e900d
Mandy <mandykparson@gmail.com>
2021-03-16 15:45:45 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    const thankYous = [];
+    for (let i = 0; i < array.length; i++) {
+        thankYous.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYous
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
+
+countDown(10);
